ff36fedbd9db81289b0a42cf9bbd625caeb98b67
unknown <alex_ohgren@hotmail.se>
2024-11-19 10:03:22 +0100

    added domain to cookie

--- routes/routes.js
+++ routes/routes.js
@@ -104,6 +104,7 @@ router.post("/login", loginLimiter, async (req, res) => {
 
     res.cookie("token", token, {
       path: "/",
+      domain: "localhost",
       maxAge: 3 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: "none",
