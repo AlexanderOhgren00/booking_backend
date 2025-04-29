aa6d8587ea49b64e291bb774b248cc733bcb97a3
AlexanderOhgren00 <alex_ohgren@hotmail.se>
2025-04-29 19:28:34 +0200

    changed email

--- routes/routes.js
+++ routes/routes.js
@@ -27,7 +27,9 @@ const paymentStates = {};
 
 // Configure nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "mailcluster.loopia.se",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
